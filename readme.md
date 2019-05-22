2ec00c76b90a346c3de1d6f5c67c6c4221e80f55
Dylan Just <dylan.just@ephox.com>
2019-05-22 15:17:37 +1000

    Include main extension source files.

---
+++ readme.md
@@ -0,0 +1,50 @@
+# TinyMCE for Contentful
+
+This repository contains the integration of the TinyMCE rich text editor into the 
+Contentful headless CMS. 
+
+Please see [this blog post](https://go.tiny.cloud/blog/is-the-headless-cms-the-future-of-content-publishing/)
+for an introduction to TinyMCE for Contentful.
+
+# Packaging
+
+This is packaged as Contentful UI Extension. The `extension.json` file defines
+the extension and points it to an external web server to serve the extension's files.
+
+The files in `src/` are deployed to:
+ - https://contentful.tiny.cloud/ (production)
+ - https://contentful-staging.tiny.cloud/ (staging)
+
+# Forking
+
+Note that this extension only works when installed using the "src" method, 
+not the "srcdoc" method. As such, if you wish to fork this extension, you 
+will need to host the files in the `src/` folder on a web server and update
+your extension.json file to point to it.
+
+# Developing
+
+To deploy experimental changes to the `extension.json` file, you can use 
+the contentful cli: 
+
+    contentful extension update --version=X
+
+where X is the previous version of the extension installed.
+This only affects your own Contentful instance.
+
+To make changes to the files in `src/`, re-deploy them to the appropriate web server. 
+
+Note that you will need to change the extension settings in the Contentful UI
+to set your API Key, and to change between the live and staging URLs.
+
+Note that whenever you `contentful extension update` it will forget your settings,
+so you'll have to put in your API key and staging URL again.
+
+# Extension URL
+
+To install the extension into Contentful, add an extension using the 
+"Install from Github" option, and enter the following URL: 
+
+https://github.com/tinymce/tinymce-contentful/blob/master/extension.json
+
+
---
+++ src/tinymce-for-contentful.js
@@ -0,0 +1,93 @@
+window.contentfulExtension.init(function(api) {
+  function tinymceForContentful(api) {
+    api.window.startAutoResizer();
+
+    function tweak(param) {
+      var t = param.trim();
+      if (t === "false") {
+        return false;
+      } else if (t === "") {
+        return undefined;
+      } else {
+        return t;
+      }
+    }
+
+    var tb = tweak(api.parameters.instance.toolbar);
+    var mb = tweak(api.parameters.instance.menubar);
+
+    tinymce.init({
+      selector: "#editor",
+      plugins: api.parameters.instance.plugins,
+      toolbar: tb,
+      menubar: mb,
+      max_height: 500,
+      min_height: 300,
+      autoresize_bottom_margin: 15,
+      resize: false,
+      init_instance_callback : function(editor) {
+        var listening = true;
+
+        function getEditorContent() {
+          return editor.getContent() || '';
+        }
+
+        function getApiContent() {
+          return api.field.getValue() || '';
+        }
+
+        function setContent(x) {
+          var apiContent = x || '';
+          var editorContent = getEditorContent();
+          if (apiContent !== editorContent) {
+            //console.log('Setting editor content to: [' + apiContent + ']');
+            editor.setContent(apiContent);
+          }
+        }
+
+        setContent(api.field.getValue());
+
+        api.field.onValueChanged(function(x) {
+          if (listening) {
+            setContent(x);
+          }
+        });
+
+        function onEditorChange() {
+          var editorContent = getEditorContent();
+          var apiContent = getApiContent();
+
+          if (editorContent !== apiContent) {
+            //console.log('Setting content in api to: [' + editorContent + ']');
+            listening = false;
+            api.field.setValue(editorContent).then(function() {
+              listening = true;
+            }).catch(function(err) {
+              console.log("Error setting content", err);
+              listening = true;
+            });
+          }
+        }
+
+        var throttled = _.throttle(onEditorChange, 500, {leading: true});
+        editor.on('change keyup setcontent blur', throttled);
+      }
+    });
+  }
+
+  function loadScript(src, onload) {
+    var script = document.createElement('script');
+    script.setAttribute('src', src);
+    script.onload = onload;
+    document.body.appendChild(script);
+  }
+
+  var sub = location.host == "contentful.staging.tiny.cloud" ? "cloud-staging" : "cloud";
+  var apiKey = api.parameters.installation.apiKey;
+  var channel = api.parameters.installation.channel;
+  var tinymceUrl = "https://" + sub + ".tinymce.com/" + channel + "/tinymce.min.js?apiKey=" + apiKey;
+
+  loadScript(tinymceUrl, function() {
+    tinymceForContentful(api);
+  });
+});
